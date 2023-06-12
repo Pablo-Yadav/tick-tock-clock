08d2f7364c1a34295ba84b610fb049c5552f90dd
Naidu Teja <naidu.teja@ZL-BLR-MAC578.local>
2023-06-12 14:12:42 +0530

    pavanteja

--- pavan/clock.js
+++ pavan/clock.js
@@ -9,9 +9,9 @@ function updateClock() {
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
 
-    const hourRotation = (hours * 30) + (minutes * 0.5) +90; 
-    const minuteRotation = (minutes * 6) + (seconds * 0.1) +90; 
-    const secondRotation = seconds * 6 +90; 
+    const hourRotation = (hours * 30) + (minutes * 0.5) ; 
+    const minuteRotation = (minutes * 6) + (seconds * 0.1) ; 
+    const secondRotation = seconds * 6 ; 
 
     hourHand.style.transform = `rotate(${hourRotation}deg)`;
     minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
